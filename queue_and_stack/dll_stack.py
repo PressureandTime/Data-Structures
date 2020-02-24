fb30fff499b97dd4b25dcaae0119ce3f51cd6e5c
Petar Scepanovic <hiborija@gmail.com>
2020-02-24 16:38:48 +0100

    finished the queue, tests are passing

--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -4,9 +4,11 @@
 
 import sys
 import os
-sys.path.append(os.path.join(os.path.dirname(os.path.abspath(
-    __file__)), '../doubly_linked_list'))
+sys.path.append(
+    os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                 '../doubly_linked_list'))
 from doubly_linked_list import DoublyLinkedList
+
 
 class Stack:
     def __init__(self):
@@ -23,7 +25,6 @@ class Stack:
             value = self.storage.remove_from_head()
             self.size -= 1
             return value
-
 
     def len(self):
         return self.storage.__len__()
